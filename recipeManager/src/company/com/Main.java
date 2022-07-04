46c4d5739abd39013f6628abde9a2385a8b5b3b3
Ahmad Al Khateeb <al.khateeb.ahmad96@gmail.com>
2022-07-04 15:32:29 +0200

    Ready for first review

--- recipeManager/src/company/com/Main.java
+++ recipeManager/src/company/com/Main.java
@@ -1,36 +1,12 @@
 package company.com;
 
-
-import java.io.*;
-
+import company.com.userView.Person;
 
 public class Main {
     public static void main(String[] args) {
 
-        BufferedReader reader;
-        BufferedWriter writer = null;
-        String fileName = "myFile";
-
-        try {
-
-            writer = new BufferedWriter(new FileWriter(fileName+".txt", true));
-            reader = new BufferedReader(new FileReader("myFile.txt"));
-
-
-            writer.write(" 1!!!\n");
-            writer.write(" 2!!!\n");
-            writer.write(" 3!!!\n");
-            writer.close();
-
-            reader.lines().forEach(System.out::println);
-            reader.close();
-
-        } catch (IOException e) {
-        }
-
-
-//        Person p = new Person();
-//        p.printBanner();
-//        p.sessionLoop();
+        Person p = new Person();
+        p.printBanner();
+        p.sessionLoop();
     }
 }
